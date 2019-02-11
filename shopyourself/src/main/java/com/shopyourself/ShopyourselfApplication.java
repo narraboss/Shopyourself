0450f309314e043fbefbe575af2c45371314f318
narraboss <basanth.narra@gmail.com>
2019-02-11 00:00:21 -0500

    initial commit

---
+++ shopyourself/src/main/java/com/shopyourself/ShopyourselfApplication.java
@@ -0,0 +1,14 @@
+package com.shopyourself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopyourselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopyourselfApplication.class, args);
+	}
+
+}
+
---
+++ shopyourself/src/test/java/com/shopyourself/ShopyourselfApplicationTests.java
@@ -0,0 +1,17 @@
+package com.shopyourself;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopyourselfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
